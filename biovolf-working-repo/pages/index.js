1ea1468979cefcfac678b4b74030b3959881d444
bvolf <bvolf04@gmail.com>
2025-04-23 21:34:37 +0200

    Add files via upload

---
+++ biovolf-working-repo/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>BioVolf Webshop</h1>
+      <p>Ez a működő kezdőoldal. Innen indulhat a rendelés.</p>
+    </main>
+  );
+}
